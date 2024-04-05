585345355c637c0a2a56c93fbb12271b252dd9c3
jonathan-gut <jonathangutierrezmcc@gmail.com>
2024-04-05 00:59:15 -0400

    changed 11.sol

--- dataset/access_control/11.sol
+++ dataset/access_control/11.sol
@@ -1,4 +1,9 @@
+pragma solidity ^0.8.0;
+
+contract Wrapped11 {
 function transferTo_txorigin11(address to, uint amount,address owner_txorigin11) public {
   require(tx.origin == owner_txorigin11);
-  to.call.value(amount);
+  to.call{value:amount}("");
+}
+
 }
